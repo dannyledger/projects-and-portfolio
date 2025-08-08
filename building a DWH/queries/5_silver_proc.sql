bec624a3adef4d1859d3e3a9f9f91e19b8c9ea6a
Danny Ledger <dannycledger@gmail.com>
2025-08-08 19:49:36 +1200

    updates

--- building a DWH/queries/5_silver_proc.sql
+++ building a DWH/queries/5_silver_proc.sql
@@ -111,3 +111,14 @@ GO
 ------------------------------------------
 -- Table 3: 
 
+SELECT
+    sls_ord_num,
+    sls_prd_key,
+    sls_cust_id,
+    sls_order_dt,
+    sls_ship_dt,
+    sls_due_dt,
+    sls_sales,
+    sls_quantity,
+    sls_price
+FROM bronze.crm_sales_details;
